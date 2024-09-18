c828cccb4933f390fe4406eb5d8f739f0f72f2a1
Chintana Luu <luucy003@mymail.unisa.edu.au>
2024-09-18 22:49:15 +0930

    Load Drinks Data From API

---
+++ server.js
@@ -0,0 +1,11 @@
+// // Import necessary modules
+
+// import dotenv from 'dotenv';
+
+// dotenv.config()
+
+// // // Hide API key in environment.
+// import 'dotenv/config'
+// require('dotenv').config();
+// console.log(process.env.HIDDEN_API_KEY);
+// const API_URL= "https://api.spoonacular.com/recipes/complexSearch?apiKey=HIDDEN_API_KEY&type=Drink"
