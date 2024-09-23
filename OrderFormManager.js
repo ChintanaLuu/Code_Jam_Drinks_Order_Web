df85d9ab7b30f5b38c41d54b86a98ed8f08ccd47
ChintanaLuu <luucy003@mymail.unisa.edu.au>
2024-09-23 21:38:44 +0930

    Install Nodejs and Requirejs
    Install Nodejs and Requirejs to run Javascript files and later fix Dotenv setup.

---
+++ OrderFormManager.js
@@ -0,0 +1,5 @@
+function checkInputFields(firstName, lastName, address) {
+    console.log(firstName, lastName, address, "HELLO WORLD!")
+}
+
+console.log("hello world!");
--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-import dotenv from 'dotenv'
+// // import dotenv from 'dotenv'
+// import 'dotenv/config'
+// // require("dotenv").config(); // Require is not defined in ES module scope, you can use import instead.
+// console.log(process.env.HIDDEN_API_KEY); // prints undefined.
+    
+import 'dotenv/config'
 require("dotenv").config();
 console.log(process.env.HIDDEN_API_KEY);
+console.log("poop");
