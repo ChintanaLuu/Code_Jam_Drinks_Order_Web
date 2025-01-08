a0d84dc50527503b06c2dd1e7f168f25b3c52363
Chintana Luu <luucy003@mymail.unisa.edu.au>
2025-01-08 16:22:24 +1030

    Add Main Page Hover Effect

---
+++ styles.js
@@ -0,0 +1,34 @@
+function updateHoverInfo(){
+
+    console.log("hello hello hello hello");
+
+//     // Fetch drink info using API and display it.
+
+
+//   const API_URL= "https://api.spoonacular.com/recipes/complexSearch?apiKey=&type=Drink";
+  
+//   fetch(API_URL)
+//   .then(res => {
+//     if (!res.ok) {
+//         throw new Error('Network response was not ok'); // Fetch returns nothing due to no wifi connection
+//     }
+//     return res.json(); // returns entire OK response converted to json. no network error.
+
+//   })
+  
+//   .then(data => {
+
+//     const results = data.results;
+    
+//     results.forEach(drink => {
+//         const drinkInfo = document.QuerySelector(`#drinkInfo-${drink.id}`);
+//         drinkInfo.innerHTML = data.drinkInfo;
+        
+        
+//     });
+//         })
+//         .catch(error => {
+//           console.error('Error:', error);
+//         });
+        
+}
