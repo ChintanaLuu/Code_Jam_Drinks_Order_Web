f6d7d17a59731b9f9c3af7d945d65a9c17fea68c
Chintana Luu <luucy003@mymail.unisa.edu.au>
2025-01-08 22:05:20 +1030

    Add Drink Info OnHover

--- styles.js
+++ styles.js
@@ -1,34 +1,40 @@
-function updateHoverInfo(){
+function updateHoverInfo(id){
 
     console.log("hello hello hello hello");
 
-//     // Fetch drink info using API and display it.
+    const drinkID = id;
 
-
-//   const API_URL= "https://api.spoonacular.com/recipes/complexSearch?apiKey=&type=Drink";
-  
-//   fetch(API_URL)
-//   .then(res => {
-//     if (!res.ok) {
-//         throw new Error('Network response was not ok'); // Fetch returns nothing due to no wifi connection
-//     }
-//     return res.json(); // returns entire OK response converted to json. no network error.
-
-//   })
-  
-//   .then(data => {
-
-//     const results = data.results;
+    // Fetch drink info using API and display it.
+    // Store API results later.
     
-//     results.forEach(drink => {
-//         const drinkInfo = document.QuerySelector(`#drinkInfo-${drink.id}`);
-//         drinkInfo.innerHTML = data.drinkInfo;
-        
-        
-//     });
-//         })
-//         .catch(error => {
-//           console.error('Error:', error);
-//         });
-        
+    const API_URL = `https://api.spoonacular.com/recipes/${drinkID}/information?apiKey=`;
+
+    fetch(API_URL)
+    .then(res => {
+    if (!res.ok) {
+        throw new Error('Network response was not ok'); // Fetch returns nothing due to no wifi connection
+    }
+    return res.json(); // returns entire OK response converted to json. no network error.
+
+    })
+
+    .then(data => {
+
+    // It already has some things like likes, vegan, etc.....
+        const drink = data.results; // undefined...
+        console.log("the drink", drink);
+
+        // drink.drinkInfo = `Vegan: ${drink.vegan}, Gluten: ${drink.glutenFree}, Dairy: ${drink.dairyFree}`;
+
+        const infoElement = document.querySelector(`#drinkInfo-${drink.id}`);
+        infoElement.innerHTML = drink.drinkInfo; // It updates and adds a text but becomes undefined after...
+        // NEED TO MAKE IT CHANGE BACK TO NO TEXT AFTER MOUSE NOT OVER ANYMORE.
+
+    })
+
+
+    .catch(error => {
+        console.error('Error:', error); // cannot read properties of undefined (reading 'id).
+    });
+    
 }
